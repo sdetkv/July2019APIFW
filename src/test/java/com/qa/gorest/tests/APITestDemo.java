54e7ae671863a3a8c14b9f02f36139ea56f9a58b
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2019-09-18 08:33:11 +0530

    sample api code

---
+++ src/test/java/com/qa/gorest/tests/APITestDemo.java
@@ -0,0 +1,15 @@
+package com.qa.gorest.tests;
+
+import org.testng.annotations.Test;
+
+public class APITestDemo {
+	
+	
+	@Test
+	public void myTest(){
+		System.out.println("my test");
+	}
+	
+	
+
+}
